c57d129a8e790f6bee28f083bfd6dde6117c58e7
azooz95 <aziz.alhaj30@gmail.com>
2025-04-12 15:26:43 +0300

    remve the api key from the script

--- src/utils/llm_api.py
+++ src/utils/llm_api.py
@@ -14,7 +14,7 @@ load_dotenv()
 gemni_api_key = os.getenv("GEMINI_API_KEY")
 
 def llm_api(contents:Optional[str] = None, wanted_information: Optional[str] = None):
-    client = genai.Client(api_key="AIzaSyCNc36mj_ctT_YVRpZmHNH4ilEKpCnk8Ac")
+    client = genai.Client(api_key=gemni_api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=[f"Extract {wanted_information} from the following text: {contents}. Return the output in valid JSON format. If any information is missing, use null"])
